d6d6ac0c17c828cf582f07afbadb422ceb342ce3
Bu Sun Kim <busunkim@google.com>
2021-03-12 00:21:11 +0000

    chore: treat docs warnings as errors

--- synth.py
+++ synth.py
@@ -99,6 +99,9 @@ templated_files = common.py_library(
 )
 s.move(templated_files, excludes=[".coveragerc"])  # microgenerator has a good .coveragerc file
 
+# Don't treat docs (sphinx) warnings as errors.
+s.replace("noxfile.py", '[\"\']-W[\"\']', '# "-W"')
+
 # ----------------------------------------------------------------------------
 # Samples templates
 # ----------------------------------------------------------------------------
