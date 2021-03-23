ba32c3cab9b784faef8179d5b45d0e9cce35d820
yoshi-automation <yoshi-automation@google.com>
2021-03-23 16:14:51 -0700

    chore: update gapic-generator-python
    PiperOrigin-RevId: 355923884
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Fri Feb 5 14:04:52 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 5e3dacee19405529b841b53797df799c2383536c
    Source-Link: https://github.com/googleapis/googleapis/commit/5e3dacee19405529b841b53797df799c2383536c

--- google/cloud/monitoring_v3/services/service_monitoring_service/pagers.py
+++ google/cloud/monitoring_v3/services/service_monitoring_service/pagers.py
@@ -15,7 +15,16 @@
 # limitations under the License.
 #
 
-from typing import Any, AsyncIterable, Awaitable, Callable, Iterable, Sequence, Tuple
+from typing import (
+    Any,
+    AsyncIterable,
+    Awaitable,
+    Callable,
+    Iterable,
+    Sequence,
+    Tuple,
+    Optional,
+)
 
 from google.cloud.monitoring_v3.types import service
 from google.cloud.monitoring_v3.types import service_service
