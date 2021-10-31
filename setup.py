33400f6629b8fa8ae6f34d79c1ad0098581b090d
Anthonios Partheniou <partheniou@google.com>
2021-10-31 09:55:40 +0000

    fix(deps): drop packaging dependency

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ dependencies = [
     # https://github.com/googleapis/google-cloud-python/issues/10566
     "google-api-core[grpc] >= 1.28.0, <3.0.0dev",
     "proto-plus >= 1.15.0",
-    "packaging >= 14.3",
 ]
 extras = {"pandas": "pandas >= 0.17.1"}
 
